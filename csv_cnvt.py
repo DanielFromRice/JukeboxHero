ab9ad5e80cea4e9c2231d87d3743503685a9dbe6
Daniel Rothfusz <dpr7@rice.edu>
2021-05-11 11:28:41 -0500

    Formatted csv parser working

--- csv_cnvt.py
+++ csv_cnvt.py
@@ -1,21 +1,57 @@
 import numpy as np
+import sys
 
-input = np.genfromtxt("sampledata.csv", delimiter = ',').astype(np.int16)
 
-print(input)
-print(input.tobytes()) # option 1
-
-with open("out.bin", 'wb') as fil: # if we want as all 16bit
-    fil.write(input.tobytes())
+if len(sys.argv) != 4:
+    raise Exception("Please run with format: python3 csv_cnvt.py [beambreak].csv [notes].csv [out].bin")
 
 bytes_ip = bytearray([])
 
-for num in input:
-    val = int(num).to_bytes((int(num).bit_length() + 7) // 8, byteorder='little')
-    print(val)
+# BEGIN READING BEAM PRELOAD ARRAY
+
+input_beams = np.genfromtxt(sys.argv[1], delimiter= ',').astype(np.int)
+
+if input.shape[1] != 4:
+    raise Exception("Incorrect shape")
+
+size = input_beams.shape[0]
+print(f"size: {size}")
+bytes_ip.append(size)
+
+for row in range(input_beams.shape[0]):
+    val = (8 * input_beams[row, 0]) + (4 * input_beams[row,1]) + (2 * input_beams[row,2]) + input_beams[row,3]
+    print(f"beam val: {val}")
+    bytes_ip.append(val)
+
+# BEGIN READING IN NOTE PACKETS
+
+input = np.genfromtxt(sys.argv[2], delimiter = ',').astype(np.int16)
+
+if input.shape[1] != 6:
+    raise Exception("Incorrect shape")
+
+size = input.shape[0]
+print(f"size: {size}")
+bytes_ip.append(size)
+
+for row in range(input.shape[0]):
+    val = int(input[row,0]).to_bytes(1, 'little')
+    print(f"val: {input[row,0]}, hex: {val}")
     bytes_ip.extend(val)
+    for col in range(1, 5):
+        val = int(input[row,col]).to_bytes(2, 'little')
+        print(f"val: {input[row,col]}, hex: {val}")
+        bytes_ip.extend(val)
+    val = int(input[row,5]).to_bytes(1, 'little')
+    print(f"val: {input[row,5]}, hex: {val}")
+    bytes_ip.extend(val)
+
+# for num in input:
+#     val = int(num).to_bytes((int(num).bit_length() + 7) // 8, byteorder='little')
+#     print(val)
+#     bytes_ip.extend(val)
 
 print(bytes_ip)
 
-with open("out2.bin", "wb") as fil: # if we want as mixed size
+with open(sys.argv[3], "wb") as fil: # if we want as mixed size
     fil.write(bytes_ip)
